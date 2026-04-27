f077df58d8f11da563a4a0e538289631e2b8a679
Avery Allison <averyallison02@gmail.com>
2026-04-26 19:24:01 -0700

    added potential errors for job::io

--- src/jobs/io.rs
+++ src/jobs/io.rs
@@ -1,2 +1,22 @@
+use std::fmt;
 use std::path::Path;
 use super::Job;
+
+use JobIOError::*;
+enum JobIOError
+{
+    CircularDependency(String, String),
+    UnexpectedToken(String, u32),
+}
+
+impl fmt::Display for JobIOError
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
+    {
+        match self
+        {
+            CircularDependency(dep1, dep2) => write!(f, "jobs {} and {} depend on each other", dep1, dep2),
+            UnexpectedToken(token, line) => write!(f, "unexpected token {} on line {}", token, line)
+        }
+    }
+}
